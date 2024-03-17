84ec4fa5a1e7cdfad179d7f83c79f85bf67d2aa2
Anton_Wombat <153746663+Anton119@users.noreply.github.com>
2024-03-17 21:39:23 +0300

    remaster of the game
    Started to rebuild the game with more convenient interface and minimax algorithm

--- tictactoe.py
+++ tictactoe.py
@@ -1,108 +1,42 @@
-# Когда законится луп gameRunning?
-import random 
-board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
-currentPlayer = "X"
-winner = None 
-gameRunning = True 
-
-
-# printing the game board 
-
-def printBoard (board):
-    print(board[0] + "|" + board[1] + "|" + board[2])
-    print("------")
-    print(board[3] + "|" + board[4] + "|" + board[5])
-    print("------")
-    print(board[6] + "|" + board[7] + "|" + board[8])
-
-# take player input 
-def playerInput(board):
-    inp = int(input("Entre a coordinate to make a move: "))
-    if inp >= 1 and inp <=9 and board[inp-1] == "-":
-        board[inp-1] = currentPlayer
-    elif inp < 1 or inp > 9:
-            print("This spot doesn't exist!")
-    else:
-        print("You cannot make a move, the spot is occupied")
+def main():
+   print("Welcome to tictactoe game!")
+   start_game()
 
 
 
-# check for win or tie
-def checkHorizontale(board):
-    global winner 
-    if board[0] == board[1] == board[2] and board[1] != "-":
-        winner = board[0]
-        return True
-    elif board[3] == board[4] == board[5] and board[3] != "-":
-        winner = board[3]
-        return True
-    elif board[6] == board[7] == board[8] and board[6] != "-":
-        winner = board[6]
-        return True
+## игровое поле 
+board = [1,2,3,4,5,6,7,8,9]
 
-def checkRows(board):
-    global winner 
-    if board[0] == board[3] == board[6] and board[0] != "-":
-        winner = board[0]
-        return True
-    elif board[1] == board[4] == board[7] and board[1] != "-":
-        winner = board[1]
-        return True
-    elif board[2] == board[5] == board[8] and board[2] != "-":
-        winner = board[2]
-        return True
+## количество клеток 
+board_size = 3
 
-def checkDiagonale(board):
-    global winner 
-    if board[0] == board[4] == board[8] and board[0] != "-":
-        winner = board[0]
-        return True 
-    elif board[6] == board [4] == board[2] and board[6] != "-":
-        winner = board[6]
-        return True 
+## вывод игрового поля 
+def draw_board():
+   print("_" * 4 * board_size)
+   for i in range(board_size):
+      print((" " * board_size + "|")*3)
+      print("",board[i*board_size], "|", board[1+i*board_size],"|",  board[2+i*board_size],"|")
+      print(("_" * board_size + "|")*board_size)
+
+## выполняем ход
+def game_step():
+    pass 
     
-def checkTie(board):
-    global gameRunning
-    if "-" not in board:
-        printBoard(board)
-        print("Tie!")
-        gameRunning = False 
-
-def checkforWin():
-    if checkDiagonale(board) or checkHorizontale(board) or checkRows(board):
-        print(f"The winner is {winner}")
-        gameRunning = False 
+    
+def check_win():
+    pass 
 
 
-# switch the player 
-def switchPlayer():
-    global currentPlayer
-    if currentPlayer == "X":
-        currentPlayer = "O" 
-    else: 
-        currentPlayer = "X"
-        
-# computer
-def computer(board):
-    while currentPlayer == "O":
-        position = random.randint(0, 8)
-        if board[position] == "-":
-            board[position] = "O"
-            switchPlayer()
+def start_game():
+    currentPlayer = "X"
+    # номре шага
+    step = 1
+    draw_board()
+    
+    while (step<10):
+       indexx = input("Current player is " + currentPlayer + 
+       ". Type the number of the coordinate where you want to make a move (0 - exit the game): ")
+       step+=1
+    
 
-# check for win or tie again
-
-
-# game
-while gameRunning:
-    printBoard(board)
-    playerInput(board)
-    checkforWin()
-    checkTie(board)
-    switchPlayer()
-    computer(board)
-    # check for win or tie again
-    checkforWin()
-    checkTie(board)
+main()
