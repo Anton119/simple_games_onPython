e0d245f176b26280d5b9a910488c7269b02784c5
Anton_Wombat <153746663+Anton119@users.noreply.github.com>
2024-05-28 21:57:34 +0300

    created turtle race

--- turtle_game.py
+++ turtle_game.py
@@ -1,6 +1,67 @@
-def get_number_of_racers ():
-    racers = 0
-    while True:
-        racers = input("Enter the number of racers (2-10): ")
+import turtle 
+import random 
+import time 
+WIDTH, HEIGHT = 500, 500
+COLORS = ["pink", "black", "purple", "blue", "green", "yellow", "orange", "red", "gray", "brown"]
 
-get_number_of_racers()
+def race(colors):
+    turtles = create_many_turtles(colors)
+    
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+            
+            x,y = racer.pos()
+            if y >= HEIGHT:
+                return colors[turtles.index(racer)]
+            
+        
+        
+
+def create_many_turtles(colors):
+    turtles = []
+    dislocation = WIDTH // (len(colors)+1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape("turtle")
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i+1) * dislocation , -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+        
+        #time.sleep(3)
+    return turtles
+
+def create_turtle():
+    screen  = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Turtle racing!")
+    time.sleep(5)
+
+def amount_of_racers():
+    while True:
+       players = input("How many players do you want to create (2-10)?: ") 
+       if players.isdigit():
+          racers = int(players)
+          if 2 <= racers <= 10:
+              return racers
+          else:
+              print("Sorry, invalid number")
+       else:
+           print("You should type a number")
+           
+             
+    
+#amount_of_racers()
+#create_turtle()
+racers = amount_of_racers()
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+winner = race(colors)
+print("The winner is the turtle with color:", winner)
+
+time.sleep(5)
+
