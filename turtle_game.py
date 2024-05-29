59f83c274c39cde420f5acfe0820390d65c87bc4
Anton_Wombat <153746663+Anton119@users.noreply.github.com>
2024-05-29 19:44:12 +0300

    made a pink turtle the biggest in size

--- turtle_game.py
+++ turtle_game.py
@@ -1,67 +1,74 @@
 import turtle 
-import random 
 import time 
+import random
+
 WIDTH, HEIGHT = 500, 500
-COLORS = ["pink", "black", "purple", "blue", "green", "yellow", "orange", "red", "gray", "brown"]
+COLORS = ["pink", "pink", "pink", "pink", "gray", "brown", "red", "green", "blue", "orange"]
+
 
 def race(colors):
-    turtles = create_many_turtles(colors)
+    
+    racers  = create_many_turtles(colors)
     
     while True:
-        for racer in turtles:
+        for turtle in racers:
             distance = random.randrange(1, 20)
-            racer.forward(distance)
+            turtle.forward(distance)
             
-            x,y = racer.pos()
-            if y >= HEIGHT:
-                return colors[turtles.index(racer)]
-            
-        
-        
-
+            x,y = turtle.pos()
+            if y >= HEIGHT - 150:
+                return colors[racers.index(turtle)]
+    
 def create_many_turtles(colors):
     turtles = []
-    dislocation = WIDTH // (len(colors)+1)
+    pinky = []
+    spacingx = WIDTH//(len(colors)+1)
     for i, color in enumerate(colors):
-        racer = turtle.Turtle()
-        racer.color(color)
-        racer.shape("turtle")
-        racer.left(90)
-        racer.penup()
-        racer.setpos(-WIDTH//2 + (i+1) * dislocation , -HEIGHT//2 + 20)
-        racer.pendown()
-        turtles.append(racer)
+        if color == "pink":
+            pinky = turtle.Turtle()
+            pinky.color(color)
+            pinky.shapesize(5)
+            pinky.left(90)
+            pinky.shape("turtle")
+            pinky.penup()
+            pinky.setpos(-WIDTH//2 + spacingx * i+1, -HEIGHT//2 + 20)
+            pinky.pendown()
+            turtles.append(pinky)
+        else:
+           racer = turtle.Turtle()
+           racer.color(color)
+           racer.shapesize(1)
+           racer.left(90)
+           racer.shape("turtle")
+           racer.penup()
+           racer.setpos(-WIDTH//2 + spacingx * i+1, -HEIGHT//2 + 20)
+           racer.pendown()
+           turtles.append(racer)
         
-        #time.sleep(3)
+        
     return turtles
 
-def create_turtle():
-    screen  = turtle.Screen()
-    screen.setup(WIDTH, HEIGHT)
+def init_turtle():
+    screen = turtle.Turtle()
+    screen = setup(WIDTH, HEIGHT)
     screen.title("Turtle racing!")
-    time.sleep(5)
 
-def amount_of_racers():
+def number_of_racers():
     while True:
-       players = input("How many players do you want to create (2-10)?: ") 
-       if players.isdigit():
-          racers = int(players)
-          if 2 <= racers <= 10:
-              return racers
-          else:
-              print("Sorry, invalid number")
-       else:
-           print("You should type a number")
-           
-             
-    
-#amount_of_racers()
-#create_turtle()
-racers = amount_of_racers()
+        racer = input("Enter the number of racers (2-10): ")
+        if racer.isdigit():
+            racer = int(racer)
+            if 2 <= racer <= 10:
+                return racer 
+            else:
+                print("Invalid number")
+                
+        else:
+            print("It's not a number, try again!")
+                
+racers = number_of_racers()
 random.shuffle(COLORS)
 colors = COLORS[:racers]
 winner = race(colors)
-print("The winner is the turtle with color:", winner)
-
-time.sleep(5)
-
+time.sleep(3)
+print("The color of the winner is:", winner)
